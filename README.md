4488a452c241196c5a068ce50a84da1aac7ea8da
David <cesarovic@seznam.cz>
2018-08-16 23:45:15 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # TeaSpeak-Updater
+
+Simple to use TeaSpeak-Updater script
+
+
+## Getting Started
+
+### Tested distros
+* Debian
+* Mint
+
+### Prerequisites
+
+* Linux distro
+* curl
+* TeaSpeak server
+
+
+### Installing
+
+* Download latest version [here](https://github.com/Najsr/TeaSpeak-Updater/archive/master.zip)
+* Extract it
+* Run it
+* Enjoy
+
+### Command Line Option
+
+* __-f__ - forces update (without prompt)
+* __-path path__ Sets working path for the script (otherwise executed in the location of the script)
---
+++ teaspeak_updater.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#TeaSpeak updater by Nicer
+#Tested on Debian
+
+#checking for parameters
+for i in "$@"
+do
+	case $i in
+   	-f|--force)
+   	FORCE="true"
+   	;;
+   	-p=*|--FOLDER=*)
+   	FOLDER="${i#*=}"
+   	;;
+   	*)
+   	;;
+	esac
+done
+
+#main
+if [ -z "$FOLDER" ]
+then
+        FOLDER="$(dirname "$(readlink -f "$0")")"
+else
+        FOLDER="$1"
+        if [[ $FOLDER == */ ]]
+        then
+                FOLDER=${FOLDER:0:(-1)}
+        fi
+fi
+
+if [ ! -f "$FOLDER/buildVersion.txt" ] 
+then
+	echo "buildVersion.txt not found, cannot proceed with update!";
+	exit 1;
+fi
+
+if [[ "$(uname -m)" == "x86_64" ]];
+then
+        arch="amd64"
+else
+        arch="x86"
+fi
+
+latest_version=$(curl -k --silent https://repo.teaspeak.de/server/linux/$arch/latest)
+current_version=$(head -n 1 "$FOLDER/buildVersion.txt")
+current_version=${current_version:11}
+
+if [[ "$latest_version" == "$current_version" ]];
+then
+   echo "UPTODATE";
+   exit 0;
+fi
+
+if [[ -z $FORCE ]];
+then
+        read -p "An update is available, do you want to update?" -n 1 -r
+        if [[ ! $REPLY =~ ^[Yy]$ ]];
+        then
+                echo "Aborting update"
+                exit 0;
+        fi
+        echo
+else
+        echo "Found new version ($latest_version), starting update"
+fi
+
+echo "Checking for running server..."
+if [[ $($FOLDER/teastart.sh status) == "Server is running" ]];
+then
+echo "Server is still running! Shutting it down..."
+$FOLDER/teastart.sh stop
+fi
+echo "Downloading server version $latest_version";
+wget -q -O /tmp/TeaSpeak.tar.gz https://repo.teaspeak.de/server/linux/$arch/TeaSpeak-$latest_version.tar.gz;
+echo "Extracting it to $FOLDER/";
+tar -C $FOLDER/ -xzf /tmp/TeaSpeak.tar.gz --overwrite
+echo "Removing temporary file";
+rm /tmp/TeaSpeak.tar.gz
+echo "UPDATED";
+exit 0;
